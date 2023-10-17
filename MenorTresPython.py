c88e5409e038bc217c2d457f32a4fcd265b9de59
SuaresDe <148288151+SuaresDe@users.noreply.github.com>
2023-10-17 19:50:38 -0300

    Add files via upload

---
+++ MenorTresPython.py
@@ -0,0 +1,13 @@
+x = int(input("Primeiro valor: "))
+y = int(input("Segundo valor: "))
+z = int(input("Terceiro valor: "))
+
+if (x < y) and (x < y):
+  print(f"MENOR = {x}")
+
+elif y < z:
+  print(f"MENOR = {y}")
+
+else:
+  print(f"MENOR = {z}")
+  
